b6cc77c3ecabaff6f8e6771e18bec337e0f9e720
Doohyun Lee <dhleekr7@gmail.com>
2021-10-02 23:41:38 +0900

    9th day

--- ss/17825.py
+++ ss/17825.py
@@ -1,4 +1,4 @@
-def dfs(pos, result, piece, test):
+def dfs(pos, result, piece):
     global ans
     if pos >= 10:
         ans = max(ans, result)
@@ -16,9 +16,7 @@ def dfs(pos, result, piece, test):
         if x == 32 or (x < 32 and x not in piece):
             before = piece[i]
             piece[i] = x
-            test.append(x)
-            dfs(pos+1, result+score[x], piece, test)
-            test.pop()
+            dfs(pos+1, result+score[x], piece)
             piece[i] = before
 
 
@@ -32,5 +30,5 @@ score = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 
 
 
 ans = 0
-dfs(0, 0, [0,0,0,0], [])
+dfs(0, 0, [0,0,0,0])
 print(ans)
