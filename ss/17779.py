0c1869fb0a3fa1a19255e3126aaff0b06ee58ddf
Doohyun Lee <dhleekr7@gmail.com>
2021-10-05 00:13:20 +0900

    11th day

--- ss/17779.py
+++ ss/17779.py
@@ -9,55 +9,44 @@ def boundary(x, y, d1, d2):
     second = 0
     third = 0
     fourth = 0
-    fifth = 0
-    visited = []
+    
+    edge = [[0]*N for _ in range(N)]
+
+    for i in range(d1+1):
+        edge[x+i][y-i] = 5
+        edge[x+d2+i][y+d2-i] = 5
+    
+    for i in range(d2+1):
+        edge[x+i][y+i] = 5
+        edge[x+d1+i][y-d1+i] = 5
 
     for i in range(x+d1):
         for j in range(y+1):
-            if i < x:
-                first += grid[i][j]
-                visited.append([i, j])
-            else:
-                if j < y-(i-x):
-                    first += grid[i][j]
-                    visited.append([i, j])
-     
+            if edge[i][j] == 5:
+                break
+            first += grid[i][j]
+        
     for i in range(x+d2+1):
-        for j in range(y+1, N):
-            if i <= x:
-                second += grid[i][j]
-                visited.append([i, j])
-            else:
-                if j >= y+1+(i-x):
-                    second += grid[i][j]
-                    visited.append([i, j])
-
-    for i in reversed(range(x+d1, N)):
+        for j in reversed(range(y+1, N)):
+            if edge[i][j] == 5:
+                break
+            second += grid[i][j]
+    
+    for i in range(x+d1, N):
         for j in range(y-d1+d2):
-            if i >= x+d1+d2:
-                third += grid[i][j]
-                visited.append([i, j])
-            else:
-                if j < y-d1+d2-((x+d1+d2) - i):
-                    third += grid[i][j]
-                    visited.append([i, j])
-      
-    for i in reversed(range(x+d2+1, N)):
-        for j in range(y-d1+d2, N):
-            if i > x+d1+d2:
-                fourth += grid[i][j]
-                visited.append([i, j])
-            else:
-                if j >= y-d1+d2+((x+d1+d2+1) - i):
-                    fourth += grid[i][j]
-                    visited.append([i, j])
+            if edge[i][j] == 5:
+                break
+            third += grid[i][j]
+    
+    for i in range(x+d2+1, N):
+        for j in reversed(range(y-d1+d2, N)):
+            if edge[i][j] == 5:
+                break
+            fourth += grid[i][j]
+    
+    fifth = sum(sum(row) for row in grid) - (first + second + third + fourth)
 
-    for i in range(N):
-        for j in range(N):
-            if [i, j] not in visited:
-                fifth += grid[i][j]
-
-    return first, second, third, fourth, fifth
+    return max(first, second, third, fourth, fifth) - min(first, second, third, fourth, fifth)
 
 N, grid = init()
 
@@ -67,7 +56,6 @@ for x in range(N-1):
         for d1 in range(1, y):
             for d2 in range(1, N-y):
                 if x+d1+d2 < N-1 and 0 <= y-d1 < y and y+d2 < N:
-                    first, second, third, fourth, fifth = boundary(x, y, d1, d2)
-                    ans = min(ans, max(first, second, third, fourth, fifth)-min(first, second, third, fourth, fifth))
+                    ans = min(ans, boundary(x, y, d1, d2))
 
 print(ans)
