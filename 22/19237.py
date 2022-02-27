1be2675421bcf20f9c86340393c5f79937e21df9
Doohyun Lee <dhleekr7@gmail.com>
2022-02-28 01:09:48 +0900

    Modify BaekJoon 19237

--- 22/19237.py
+++ 22/19237.py
@@ -1,117 +1,82 @@
-def init():
-    N, M, K = map(int, input().split())
-    shark_map = [list(map(int, input().split())) for _ in range(N)]
-
-    direction_init = list(map(int, input().split()))
-
-    priority = []
-
-    for i in range(M):
-        shark = []
-        for j in range(4):
-            temp = list(map(int, input().split()))
-            shark.append(temp)
-        priority.append(shark)
-
-    for i in range(M):
-        direction_init[i] -= 1
-        for j in range(4):
-            for k in range(4):
-                priority[i][j][k] -= 1
-
-    smell_map = [[0]*N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            shark = shark_map[i][j]
-            if shark != 0:
-                shark_map[i][j] = [shark, direction_init[shark-1]]
-                smell_map[i][j] = [shark, K]
-
-    return N, M, K, shark_map, smell_map, priority
-
-def find_shark():
-    idx = []
-    for i in range(N):
-        for j in range(N):
-            if shark_map[i][j] != 0:
-                idx.append([i, j])
-    return idx
-
-def smell_update():
-    for i in range(N):
-        for j in range(N):
-            if smell_map[i][j] != 0:
-                if smell_map[i][j][1] > 0:
-                    smell_map[i][j][1] -= 1
-                    if smell_map[i][j][1] == 0:
-                        smell_map[i][j] = 0
-                
-
-    return smell_map
-
-def shark_move():
-    shark_idx = find_shark()
-
-    update = []
-    for idx in shark_idx:
-        i, j = idx
-        shark = shark_map[i][j][0]
-        current_d = shark_map[i][j][1]
-
-        d_list = priority[shark-1][current_d]
-        done = False
-
-        for d in d_list:
-            new_i = i + di[d]
-            new_j = j + dj[d]
-
-            if 0 <= new_i < N and 0 <= new_j < N and smell_map[new_i][new_j] == 0 and shark_map[new_i][new_j] == 0:
-                update.append([i, j, new_i, new_j, shark, d])
-                done = True
-                break
-        if done:
-            continue     
-
-        for d in d_list:
-            new_i = i + di[d]
-            new_j = j + dj[d]
-
-            if  0 <= new_i < N and 0 <= new_j < N and smell_map[new_i][new_j][0] == shark:
-                update.append([i, j, new_i, new_j, shark, d])
-                done = True
-                break
-        if done:
-            continue
-
-    smell_update()
-    for u in update:
-        i, j, new_i, new_j, shark, d = u
-        if shark_map[new_i][new_j] != 0:
-            if shark < shark_map[new_i][new_j][0]:
-                shark_map[new_i][new_j], shark_map[i][j] = [shark, d], 0
-                smell_map[new_i][new_j] = [shark, K]
-            else:
-                shark_map[i][j] = 0
-        else:
-            shark_map[new_i][new_j], shark_map[i][j] = [shark, d], 0
-            smell_map[new_i][new_j] = [shark, K]
-
-N, M, K, shark_map, smell_map, priority = init()
-
 di = [-1, 1, 0, 0]
 dj = [0, 0, -1, 1]
 
+def init():
+    n, m, k = map(int, input().split())
+    grid = [list(map(int, input().split())) for _ in range(n)]
+    shark_dir = list(map(lambda x : int(x) - 1, input().split()))
+    priority = []
+    for _ in range(m):
+        temp = []
+        for _ in range(4):
+            temp.append(list(map(lambda x : int(x) - 1, input().split())))
+        priority.append(temp)
+    return n, m, k, grid, shark_dir, priority
+
+def find_idx():
+    res = []
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] != 0:
+                res.append([grid[i][j] - 1, i, j])
+    return res
+
+def move():
+    new_shark_loc = dict()
+    new_smell_map = [[b[:] for b in a] for a in smell_map] # deepcopy
+
+    for i, j in shark_loc.keys():
+        num, d = shark_loc[(i, j)]
+        for nd in priority[num][d]:
+            new_i = i + di[nd]
+            new_j = j + dj[nd]
+            if 0 <= new_i < n and 0 <= new_j < n and smell_map[new_i][new_j] == [-1, 0]: # 아무냄새 없을때
+                if new_smell_map[new_i][new_j] == [-1, 0]:
+                    new_smell_map[new_i][new_j] = [num, k + 1]
+                    new_shark_loc[(new_i, new_j)] = [num, nd]
+                else:
+                    if num < new_smell_map[new_i][new_j][0]:
+                        new_smell_map[new_i][new_j] = [num, k + 1]
+                        new_shark_loc[(new_i, new_j)] = [num, nd]
+                break   
+        else:
+            for nd in priority[num][d]:
+                new_i = i + di[nd]
+                new_j = j + dj[nd]
+                if 0 <= new_i < n and 0 <= new_j < n and smell_map[new_i][new_j][0] == num: # 자기 냄새
+                    new_smell_map[new_i][new_j] = [num, k + 1]
+                    new_shark_loc[(new_i, new_j)] = [num, nd]
+                    break
+    
+    return new_shark_loc, new_smell_map
+
+def smell_fly():
+    for i in range(n):
+        for j in range(n):
+            if smell_map[i][j] != [-1, 0]: # [상어 번호, 남은 시간]
+                num, temp = smell_map[i][j]
+                temp -= 1
+                if temp == 0:
+                    smell_map[i][j] = [-1, 0]
+                else:
+                    smell_map[i][j] = [num, temp]
+
+n, m, k, grid, shark_dir, priority = init()
+smell_map = [[[-1, 0] for _ in range(n)] for _ in range(n)]
+shark_loc = dict()
+
+for temp in find_idx():
+    num, i, j = temp
+    d = shark_dir[num]
+    shark_loc[(i, j)] = [num, d]
+    smell_map[i][j] = [num, k]
+
 t = 0
-while True:
-    shark_move()
+while len(shark_loc.keys()) > 1:
     t += 1
-
-    idx = find_shark()
-    if len(idx) == 1:
-        break
-
-    if t >= 1000:
+    shark_loc, smell_map = move()
+    smell_fly()
+    if t > 1000:
         t = -1
         break
-
 print(t)
