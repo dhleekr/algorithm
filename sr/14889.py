09455fabab71bf1e5d9d7a184cd0999d0423e40f
Doohyun Lee <dhleekr7@gmail.com>
2022-03-07 18:13:04 +0900

    Add BaekJoon 14889

--- sr/14889.py
+++ sr/14889.py
@@ -15,18 +15,19 @@ def combination(arr, r):
                     yield [arr[i]] + j
 
 
-
-
-
-
-
-
-
-
-
-
-
 n, grid = init()
 ans = 1e10
-for temp in combination([1,2,3,4], 1):
-    print(temp)
+arr = [i for i in range(n)]
+
+for start in combination(arr, n // 2):
+    link = [i for i in range(n) if i not in start]
+    start_temp = 0
+    link_temp = 0
+    for i in start:
+        for j in start:
+            start_temp += grid[i][j]
+    for i in link:
+        for j in link:
+            link_temp += grid[i][j]
+    ans = min(ans, abs(start_temp - link_temp))
+print(ans)
