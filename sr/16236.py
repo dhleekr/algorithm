3f855e66f82347af4c7ce353c11b38acd9b84148
Doohyun Lee <dhleekr7@gmail.com>
2022-03-26 14:45:46 +0900

    Add BaekJoon 16236

---
+++ sr/16236.py
@@ -0,0 +1,60 @@
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
+
+def init():
+    n = int(input())
+    grid = []
+    for i in range(n):
+        temp = list(map(int, input().split()))
+        for j in range(n):
+            if temp[j] == 9:
+                temp[j] = 2
+                shark = [i, j]
+                break
+        grid.append(temp)
+    return n, grid, shark
+
+def bfs(shark):
+    i, j = shark
+    q = [[i , j, 0]]
+    eat = []
+    visited = [[0] * n for _ in range(n)]
+    visited[i][j] = 1
+    while q:
+        i, j, dist = q.pop(0)
+        dist += 1
+        for d in range(4):
+            new_i = i + di[d]
+            new_j = j + dj[d]
+            if 0 <= new_i < n and 0 <= new_j < n and grid[shark[0]][shark[1]] >= grid[new_i][new_j] and visited[new_i][new_j] == 0:
+                visited[new_i][new_j] = 1
+                q.append([new_i, new_j, dist])
+                if 0 < grid[new_i][new_j] < grid[shark[0]][shark[1]]:
+                    eat.append([dist, new_i, new_j])
+    
+    if not eat:
+        return None
+    eat.sort()
+    return eat[0]
+
+
+n, grid, shark = init()
+ans = 0
+cnt = 0
+
+fish = bfs(shark)
+while fish:
+    dist, i, j = fish
+    cnt += 1
+    ans += dist
+    size = grid[shark[0]][shark[1]]
+    if cnt == size:
+        grid[i][j] = size + 1
+        grid[shark[0]][shark[1]] = 0
+        cnt = 0
+    else:
+        grid[i][j] = size
+        grid[shark[0]][shark[1]] = 0
+    shark = [i, j]
+    fish = bfs(shark)
+print(ans)
